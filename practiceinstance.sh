6721b4f04596e1328560b8e32369215813321fd5
Sharath3116 <sharath3192@gmail.com>
2025-05-04 09:01:09 +0530

    new script

--- practiceinstance.sh
+++ practiceinstance.sh
@@ -3,7 +3,7 @@
 AMI=ami-09c813fb71547fc4f #this keeps on changing
 SG_ID=sg-071998ddd49fffc80 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-#ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
+ZONE_ID=Z021572213MU2NIHHWTVD # replace your zone ID
 DOMAIN_NAME="olavu.in"
 
 for i in "${INSTANCES[@]}"
@@ -18,4 +18,23 @@ do
 
         IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
+#create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
 done
