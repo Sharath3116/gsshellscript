506233ab9b7896469d81d04201c9926a35ce34a9
Sharath3116 <sharath3192@gmail.com>
2025-05-04 08:37:09 +0530

    new script

---
+++ practiceinstance.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+AMI=ami-09c813fb71547fc4f #this keeps on changing
+SG_ID=sg-071998ddd49fffc80 #replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+#ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
+DOMAIN_NAME="olavu.in"
+
+for i in "${INSTANCES[@]}"
+do
+    echo "instance is $i"
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+        IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
+
+
+
+#$ aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-903004f8 --subnet-id subnet-6e7f829e
+{
